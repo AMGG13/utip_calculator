68040763d23825a3cc1627639a821446c22712de
AMGG13 <alvinmillergarciagarcia@gmail.com>
2025-09-13 17:42:48 +0200

    Clean and setup project

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UTip - Home',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const UTip(),
+    );
+  }
+}
+
+class UTip extends StatefulWidget {
+  const UTip({super.key});
+
+  @override
+  State<UTip> createState() => _UTipState();
+}
+
+class _UTipState extends State<UTip> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("UTip App"),
+        leading: const Text('Hello'),
+      ),
+      body: Column(
+        children: [Container(child: const Text("Total per person"))],
+      ),
+    );
+  }
+}
